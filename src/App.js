82a0dc3f36f05655fb203f92f0377ccd2f77b713
Nivashini <t-r.nivashini@capgemini.com>
2022-04-17 14:55:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+   const initialValues = {email: "", password: "" };
+   const [formValues, setFormValues] = useState(initialValues);
+   const [formErrors, setFormErrors] = useState({});
+   const [isSubmit, setIsSubmit] = useState(false);
+ 
+   const handleChange = (e) => {
+     const { name, value } = e.target;
+     setFormValues({ ...formValues, [name]: value });
+   };
+ 
+   const handleSubmit = (e) => {
+     e.preventDefault();
+     setFormErrors(validate(formValues));
+     setIsSubmit(true);
+   };
+ 
+   useEffect(() => {
+     console.log(formErrors);
+     if (Object.keys(formErrors).length === 0 && isSubmit) {
+       console.log(formValues);
+     }
+   }, [formErrors]);
+   const validate = (values) => {
+     const errors = {};
+     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+     if (!values.email) {
+       errors.email = "Email is required!";
+     } else if (!regex.test(values.email)) {
+       errors.email = "This is not a valid email format!";
+     }
+     if (!values.password) {
+       errors.password = "Password is required";
+     } else if (values.password.length < 4) {
+       errors.password = "Password must be more than 4 characters";
+     } else if (values.password.length > 10) {
+       errors.password = "Password cannot exceed more than 10 characters";
+     }
+     return errors;
+
+
+   };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="ui message success">Signed in successfully</div>
+      ):(<div className="ui message"></div>)
+     }
+
+      <form onSubmit={handleSubmit}>
+        <h1>Student Login</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <p>{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+             type="text" 
+             name="email" 
+             placeholder="Enter Your Email" 
+             value={formValues.email} 
+             onChange={handleChange} />
+          </div>
+          <p>{formErrors.email}</p>
+          <div className="field">
+            <label>Password</label>
+            <input 
+             type="password" 
+             name="password" 
+             placeholder="Enter Your Password" 
+             value={formValues.password} 
+             onChange={handleChange} />
+          </div>
+          <p>{formErrors.password}</p>
+          <button className='form-input-btn' type='submit'>
+          Sign up
+        </button>
+          <span className='form-input-Register'>  New Student?<a href='#'> Register Now</a></span>
+        </div>
+      </form>
     </div>
   );
 }
